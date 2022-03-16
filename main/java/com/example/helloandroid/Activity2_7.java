3e74d0e93cfb3af4d95017bee9f378bc791b2751
Noriaki77 <sepiano15@gmail.com>
2022-03-16 21:57:42 +0900

    first commit
    upload files

---
+++ main/java/com/example/helloandroid/Activity2_7.java
@@ -0,0 +1,14 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2_7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_7);
+    }
+}
